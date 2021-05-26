7643afbfe907554c7879ddfe985efd25b86d16bc
JattMannu <manpreet_firewall@yahoo.com.sg>
2021-05-26 17:11:42 +0800

    configs update

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  // 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
+  githubConvertedToken:
+    "Z2hwXzlkZzU2UWZCRVdmVUl6RFFjdUFiU1RRVUtsdURwdDFET1hoSwo=",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "JattMannu",
 
   // Your Github Repo Name Where You Have your issues as Blog
   githubRepo: "react-blog-github",
@@ -11,22 +13,22 @@ export const config = {
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: "Iv1.e72fcc50a4dc6434",
+  OAuthSecret: "775e74d7046d3bfe441703596fc34f40280e5c08",
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title: "The Coding Dream",
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle: "Software Developer",
 
   // Header customization
   header: {
-    backgroundColor: '#f1f6f8', // can be a CSS gradient
-    backgroundColorDark: '#4C566A',
-    titleColor: '#ff5252',
-    titleColorDark: '#16a085',
-    subtitleColor: '#37474f',
-    subtitleColorDark: '#D8DEE9',
+    backgroundColor: "#f1f6f8", // can be a CSS gradient
+    backgroundColorDark: "#4C566A",
+    titleColor: "#ff5252",
+    titleColorDark: "#16a085",
+    subtitleColor: "#37474f",
+    subtitleColorDark: "#D8DEE9",
   },
 };
